45f3f71583219626ee98f82ef0cbfb958329cf3e
Nakul Sharathkumar <NakulSK221B@users.noreply.github.com>
2025-04-16 01:37:39 +0530

    Landing page completed
    Added the career timeline section, projects section, contact section and footer.
    Also added the back to top button.
    Improved the dynamic scaling of the website by implementing collapsible nav bar as well.
    Also include scrollreveal plugin.-->set to 0px

--- js/script.js
+++ js/script.js
@@ -3,3 +3,21 @@ const header = document.querySelector('header');
 window.addEventListener('scroll', function() {
   header.classList.toggle('sticky', window.scrollY > 50);
 });
+
+let menu=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+menu.onclick=()=>{
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+window.onscroll=()=>{
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+const sr = ScrollReveal({
+  distance: '0px',
+  duration: 2500,
+  reset: true
+});
+sr.reveal('.home', {delay:190, origin: 'bottom' });
+sr.reveal('.about, .projects, .career, .contact', { delay:200,origin: 'bottom' }); 
